6d772acae165bfc1d8b23d0ae2b585c8a4d3a31c
KennyAle <kennyviver@gmail.com>
2024-11-16 05:22:43 -0800

    Adding slider and hover effect (Testimonials / Styles sections)

--- js/index.js
+++ js/index.js
@@ -1,32 +1,74 @@
 //slider
+const slides = [
+  ["Carla is the best! She made me the night's brightest star! She stayed with me the whole wedding ensuring I always looked and felt wonderful!!", "Janny.png"],
+  ["Carla Beauty's makeup for my photoshoot was outstanding! Their expertise and attention to detail made me look perfect in every shot. Highly recommend!", "Ingrid.png"],
+  ["Carla Beauty's party makeup service was amazing! The makeup lasted all night, keepping me flawless until the end. Thank you, Carla for an unforgettable experience!", "Marcela.png"]
+]
+
 document.addEventListener("DOMContentLoaded", function () {
-  const testimonials = document.getElementById("testimonials");
+  const testimony = document.getElementById("testimony");
+  const testimonyImg = document.getElementById("testimony-img");
   const prevButton = document.getElementById("prev");
   const nextButton = document.getElementById("next");
   let currentIndex = 0;
+  let autoSlide
 
-  const updateTestimonials = () => {
-    const width = testimonials.clientWidth;
-    testimonials.style.transform = `translateX(-${currentIndex * width}px)`;
-  };
+  function slideChange(index) {
+    testimony.classList.add('fade-out')
+    testimonyImg.classList.add('fade-out')
 
-  nextButton.addEventListener("click", function () {
-    if (currentIndex < testimonials.children.length - 1) {
-      currentIndex++;
-      updateTestimonials();
+    setTimeout(() => {
+      currentIndex = index
+      testimony.innerHTML = slides[currentIndex][0]
+      testimonyImg.src = `img/index/${slides[currentIndex][1]}`
+
+      testimony.classList.remove('fade-out')
+      testimonyImg.classList.remove('fade-out')
+      testimony.classList.add('fade-in')
+      testimonyImg.classList.add('fade-in')
+
+      setTimeout(() => {
+        testimony.classList.remove('fade-in')
+        testimonyImg.classList.remove('fade-in')
+      }, 300)
+    }, 300)
+  }
+
+  function startSlider() {
+    autoSlide = setInterval(() => {
+      let nextSlide = currentIndex + 1
+      if (nextSlide > slides.length - 1) {
+        nextSlide = 0
+      }
+      slideChange(nextSlide)
+    }, 2000);
+  }
+
+  function stopSlider() {
+    clearInterval(autoSlide)
+  }
+
+  nextButton.addEventListener("click", () => {
+    stopSlider()
+    let nextSlide = currentIndex + 1
+    if (nextSlide > slides.length - 1) {
+      nextSlide = 0
     }
+    slideChange(nextSlide)
+    startSlider()
   });
 
-  prevButton.addEventListener("click", function () {
-    if (currentIndex > 0) {
-      currentIndex--;
-      updateTestimonials();
+  prevButton.addEventListener("click", () => {
+    stopSlider()
+    let nextSlide = currentIndex - 1
+    if (nextSlide < 0) {
+      nextSlide = slides.length - 1
     }
+    slideChange(nextSlide)
+    startSlider()
   });
 
-  window.addEventListener("resize", updateTestimonials);
-
-
+startSlider()
   // Let's Talk Section 
   function toggleDisabled(inputId, checkboxId) {
     const inputElement = document.getElementById(inputId)
